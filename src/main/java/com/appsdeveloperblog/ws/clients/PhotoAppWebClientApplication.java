002299552ca6a0e03e465c40ea35e7b4205ab308
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2020-08-31 11:06:22 -0400

    initial commit

---
+++ src/main/java/com/appsdeveloperblog/ws/clients/PhotoAppWebClientApplication.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.ws.clients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppWebClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppWebClientApplication.class, args);
+	}
+
+}
