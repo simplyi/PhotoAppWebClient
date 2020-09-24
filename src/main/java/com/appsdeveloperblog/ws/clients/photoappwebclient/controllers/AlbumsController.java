d98d8eee03533f473c1a3ebad1b0c50460a77cfa
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2020-09-24 17:27:41 -0400

    Make use of WebClient in a Controller class

--- src/main/java/com/appsdeveloperblog/ws/clients/photoappwebclient/controllers/AlbumsController.java
+++ src/main/java/com/appsdeveloperblog/ws/clients/photoappwebclient/controllers/AlbumsController.java
@@ -21,6 +21,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.appsdeveloperblog.ws.clients.photoappwebclient.response.AlbumRest;
 
@@ -30,50 +31,26 @@ public class AlbumsController {
 	@Autowired
 	OAuth2AuthorizedClientService oauth2ClientService;
 	
-	@Autowired
-	RestTemplate restTemplate;
+//	@Autowired
+//	RestTemplate restTemplate;
 
+	@Autowired
+	WebClient webClient;
+	
+	
 	@GetMapping("/albums")
 	public String getAlbums(Model model, 
 			@AuthenticationPrincipal OidcUser principal) {
-		
-		Authentication authentication =  SecurityContextHolder.getContext().getAuthentication();
-		OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
-		
-		OAuth2AuthorizedClient oauth2Client = oauth2ClientService.loadAuthorizedClient(oauthToken.getAuthorizedClientRegistrationId(), 
-				oauthToken.getName());
-		
-		String jwtAccessToken = oauth2Client.getAccessToken().getTokenValue();
-		System.out.println("jwtAccessToken = " + jwtAccessToken);
-		
-		
-		System.out.println("Principal = " + principal);
-		
-		OidcIdToken idToken = principal.getIdToken();
-		String idTokenValue = idToken.getTokenValue();
-		System.out.println("idTokenValue = " + idTokenValue);
+
 		
 		String url = "http://localhost:8082/albums";
-		HttpHeaders headers = new HttpHeaders();
-		headers.add("Authorization", "Bearer " + jwtAccessToken);
-		
-		HttpEntity<List<AlbumRest>> entity = new HttpEntity<>(headers);
-		
-		
-		ResponseEntity<List<AlbumRest>> responseEntity =  restTemplate.exchange(url, HttpMethod.GET, entity, new ParameterizedTypeReference<List<AlbumRest>>() {});
- 
-		List<AlbumRest> albums = responseEntity.getBody();
-		
-//		AlbumRest album = new AlbumRest();
-//		album.setAlbumId("albumOne");
-//		album.setAlbumTitle("Album one title");
-//		album.setAlbumUrl("http://localhost:8082/albums/1");
-//		
-//		AlbumRest album2 = new AlbumRest();
-//		album2.setAlbumId("albumTwo");
-//		album2.setAlbumTitle("Album two title");
-//		album2.setAlbumUrl("http://localhost:8082/albums/2");
-// 
+
+		List<AlbumRest> albums = webClient.get()
+				.uri(url)
+				.retrieve()
+				.bodyToMono(new ParameterizedTypeReference<List<AlbumRest>>(){})
+				.block();
+	
         model.addAttribute("albums", albums);
 		
 		
