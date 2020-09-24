0aa6c870d29d475d52f2452f1716890e3785cce4
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2020-09-24 17:27:04 -0400

    Create a WebClient Bean

--- src/main/java/com/appsdeveloperblog/ws/clients/photoappwebclient/PhotoAppWebClientApplication.java
+++ src/main/java/com/appsdeveloperblog/ws/clients/photoappwebclient/PhotoAppWebClientApplication.java
@@ -3,7 +3,11 @@ package com.appsdeveloperblog.ws.clients.photoappwebclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class PhotoAppWebClientApplication {
@@ -15,6 +19,20 @@ public class PhotoAppWebClientApplication {
 	@Bean
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
+	}
+	
+	@Bean
+	public WebClient webClient(ClientRegistrationRepository clientRegistrationrepository,
+			OAuth2AuthorizedClientRepository oAuth2AuthorizedClientRepository
+			) {
+		
+		ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2 = 
+				new ServletOAuth2AuthorizedClientExchangeFilterFunction(clientRegistrationrepository, 
+						oAuth2AuthorizedClientRepository);
+		
+		oauth2.setDefaultOAuth2AuthorizedClient(true);
+		
+		return WebClient.builder().apply(oauth2.oauth2Configuration()).build();
 	}
 
 }
